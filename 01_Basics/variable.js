306f67209357ef83bd040bcafa00875491b61ae5
Ehsan ul khaliq <137249680+Ehsanulkhaliq4@users.noreply.github.com>
2024-12-11 16:08:13 +0000

    variable

---
+++ 01_Basics/variable.js
@@ -0,0 +1,6 @@
+const accountId =567890;
+let accountEmail = "ehsan@gmail.com";
+var accountPassword = "12345"
+accountCity = "Lahore";
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
