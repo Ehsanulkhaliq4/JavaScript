296d78172233dd773ed864917b34e78848be7006
Ehsan ul khaliq <137249680+Ehsanulkhaliq4@users.noreply.github.com>
2024-12-11 17:58:27 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,9 @@
+"use strick";//treat all js as newer syntex
+let num  = 4;
+let name = "Ehsan";
+var BigInt= 75757757;
+let state = null;
+console.log("Name is "+name + " and favorite number is : "+num);
+console.log(BigInt);
+console.log(state);
+
